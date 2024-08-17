c58f3e4cbeeca5f57114e2b8bab6a17a705e7144
Amir Mohebali <amir.mohebali76@gmail.com>
2024-08-17 18:32:22 +0330

    add form to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import './main.css'
 
 function App() {
-  
+  const [text, setText] = useState('')
+
   return (
-    <div>
-      App
+    <div className="app">
+      <form>
+        <textarea 
+          cols="20"
+          rows="4"
+          value={text}
+          onChange={(e)=> setText(e.target.value)}
+        >
+        </textarea>
+
+        <h4>Character Count: </h4>
+      </form>
     </div>
   )
 }
