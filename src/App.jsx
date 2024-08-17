28b868953401fd13e4dd20d949ceb7055f1132ab
Amir Mohebali <amir.mohebali76@gmail.com>
2024-08-17 19:24:44 +0330

    func/count characters in input field

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import './main.css'
 
 function App() {
   const [text, setText] = useState('')
+  const [charCounter, setCharCounter] = useState(0);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    // const chars = value.split(" ").join('');
+    const chars = value.replace(/\s/g,'');
+    setText(value);
+    setCharCounter(chars.length);
+  }
 
   return (
     <div className="app">
@@ -11,11 +20,12 @@ function App() {
           cols="20"
           rows="4"
           value={text}
-          onChange={(e)=> setText(e.target.value)}
+          onChange={handleChange}
         >
         </textarea>
 
-        <h4>Character Count: </h4>
+        <h4>Character Count: {charCounter}</h4>
+        <span><b>Note:</b> whitespace is ignored</span>
       </form>
     </div>
   )
